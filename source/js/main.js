0860c8389217f92344c3342528af9077dfee7245
Andrey Dumskiy <dumskiy-25@mail.ru>
2023-10-14 03:47:18 +0300

    Adds changes

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+import './honeycomb.js';
+
+// const navigationSite = document.querySelector('.header__navigation');
+// const headerToggle = document.querySelector('.header__toggle');
+
+// navigationSite.classList.remove('header__navigation--nojs');
+// headerToggle.classList.remove('header__toggle--nojs');
+
+// headerToggle.addEventListener('click', () => {
+//   if (headerToggle.classList.contains('header__toggle--closed')) {
+//     headerToggle.classList.remove('header__toggle--closed');
+//     headerToggle.classList.add('header__toggle--opened');
+//     navigationSite.classList.remove('header__navigation--closed');
+//     navigationSite.classList.add('header__navigation-site--opened');
+//   } else {
+//     headerToggle.classList.add('header__toggle--closed');
+//     headerToggle.classList.remove('header__toggle--opened');
+//     navigationSite.classList.add('header__navigation--closed');
+//     navigationSite.classList.remove('header__navigation--opened');
+//   }
+// })
